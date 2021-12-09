38dbcd57c7c68132ca5fdbf0204ec97477f3fd22
mark182182 <versalife11@hotmail.com>
2021-12-09 13:15:53 +0100

    Rethinking the backtracking logic + initial values

--- sudoku.js
+++ sudoku.js
@@ -1,18 +1,19 @@
 window.onload = () => {
   let currentlySelected = null;
 
+  const availableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let squares = [];
   squares.length = 81;
   squares.fill(0);
   // starting 3x3 grid indexes in a column
-  let grids = [[0, 1, 2, 9, 10, 11, 18, 19, 20], [27, 28, 29, 36, 37, 38, 45, 46, 47], [54, 55, 56, 63, 64, 65, 72, 73, 74]]; 
+  let grids = [[0, 1, 2, 9, 10, 11, 18, 19, 20], [27, 28, 29, 36, 37, 38, 45, 46, 47], [54, 55, 56, 63, 64, 65, 72, 73, 74]];
   // ordering of the columns of grids inside the whole square 
   const squareColumns = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
   // ordering of the columns inside a grid
   const gridColumns = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
-  
+
   let generatedSquares = [];
-  let hasConflict = false;
+  let conflictingIndexes = [];
 
   const generateGridsInColumn = (addIndex) => {
     let tempGrid = [];
@@ -26,7 +27,7 @@ window.onload = () => {
     });
     return tempGrid;
   }
-  
+
   const secondColumn = generateGridsInColumn(3);
   const thirdColumn = generateGridsInColumn(6);
   grids = [...grids, ...secondColumn];
@@ -35,7 +36,10 @@ window.onload = () => {
   // TODO since there are three main logical blocks that handle the validation,
   // they can be extracted to different threads for faster performance
   const validate = (index, value) => {
-    let isValid = true;
+    conflictingIndexes = [];
+    console.log(squares);
+    console.log('Validating for index:' + index);
+
     let foundGrid = [];
     let gridIndex = null;
 
@@ -46,36 +50,29 @@ window.onload = () => {
       }
     })[0];
 
-   console.log(foundGrid);
-    
     // check if the value is present in the grid
     const indexInFoundGrid = foundGrid.indexOf(index);
-    console.log(squares);
     foundGrid.forEach(indexInGrid => {
-      console.log('Not the current index:' + indexInGrid != index);
-      console.log('Value in squares: ' + squares[indexInGrid] + ', current value: ' + value);
-      if (indexInGrid != index && squares[indexInGrid] === value) {
-        isValid = false;
+      if (indexInGrid != index && squares[indexInGrid] !== 0 && squares[indexInGrid] === value) {
+        console.log('Grid conflict:' + indexInGrid);
+        conflictingIndexes.push(index);
       }
     });
 
     // check if the value is present in the column
     let columnIndexesToCheck = [];
     let columnNumIndexesInGrid = [indexInFoundGrid];
-    
+
     const firstColumnNumIndexInGrid = indexInFoundGrid + 3 > 8 ? indexInFoundGrid + 3 - 9 : indexInFoundGrid + 3;
     const secondColumnNumIndexInGrid = indexInFoundGrid + 6 > 8 ? indexInFoundGrid + 6 - 9 : indexInFoundGrid + 6;
+
+    columnIndexesToCheck = [...columnIndexesToCheck, foundGrid[firstColumnNumIndexInGrid]];
+    columnIndexesToCheck = [...columnIndexesToCheck, foundGrid[secondColumnNumIndexInGrid]];
 
     columnNumIndexesInGrid = [...columnNumIndexesInGrid, firstColumnNumIndexInGrid, secondColumnNumIndexInGrid];
 
     console.log('Column indexes in grid: ' + columnNumIndexesInGrid);
-    console.log('Found grid: ' + foundGrid);
-
-    columnNumIndexesInGrid.forEach(idx => {
-      columnIndexesToCheck = [...columnIndexesToCheck, foundGrid[idx]];
-    });
     
-    console.log('Indexes in the current grid: ' + columnIndexesToCheck);
     const currentGridsInColumn = squareColumns.filter(column => column.includes(gridIndex))[0];
     currentGridsInColumn.forEach(otherGridIndex => {
       if (otherGridIndex != gridIndex) {
@@ -86,25 +83,25 @@ window.onload = () => {
       }
     });
 
+    console.log(`Column indexes to check: ${columnIndexesToCheck}`);
+    
     const columnVals = [];
     columnIndexesToCheck.forEach(indexToCheck => {
-      console.log(indexToCheck);
-      console.log(squares[indexToCheck]);
       columnVals.push(squares[indexToCheck]);
-      if (squares[indexToCheck] === value) {
-        isValid = false;
+      if (indexToCheck != index && squares[indexToCheck] !== 0 && squares[indexToCheck] === value) {
+        console.log('Column conflict:' + indexToCheck);
+        conflictingIndexes.push(indexToCheck);
       }
     });
-
-    console.log(`Column indexes to check: ${columnIndexesToCheck}`);
+    
     console.log(columnVals);
-  
+
     // check if the value is present in the row
     const indexInGridColumns = gridColumns.filter(column => column.includes(indexInFoundGrid))[0].indexOf(indexInFoundGrid);
 
     let rowIndexesToCheck = [];
     let rowNumIndexesInGrid = [indexInFoundGrid];
-    
+
     gridColumns.forEach(column => {
       if (!column.includes(indexInFoundGrid)) {
         let indexToCheck = foundGrid[column[indexInGridColumns]];
@@ -126,50 +123,38 @@ window.onload = () => {
         });
       };
     });
-    
+
     console.log(`Row indexes to check: ${rowIndexesToCheck}`);
 
     const rowVals = [];
     rowIndexesToCheck.forEach(indexToCheck => {
       rowVals.push(squares[indexToCheck]);
-      if (squares[indexToCheck] === value) {
-        isValid = false;
+      if (indexToCheck != index && squares[indexToCheck] !== 0 && squares[indexToCheck] === value) {
+        console.log('Row conflict:' + indexToCheck);
+        conflictingIndexes.push(index);
       }
     });
 
     console.log(rowVals);
-    
-    return isValid;
+    if (conflictingIndexes.length > 0) {
+      const confValues = conflictingIndexes.map(idx => squares[idx]);
+      console.log('Conflicting indexes: ' + conflictingIndexes + ', values: ' + confValues);
+    }
+
+    return conflictingIndexes.length === 0;
   }
 
-  const generateRandNumber = (squareIdx, tempNums, invalidNums) => {
-    const randNum = tempNums[Math.floor(Math.random() * tempNums.length)];
+  const generateRandNumber = (squareIdx, availableNums, invalidNums) => {
+    const randNum = availableNums[Math.floor(Math.random() * availableNums.length)];
     if (invalidNums.includes(randNum)) {
-      return generateRandNumber(squareIdx, tempNums, invalidNums);
+      return generateRandNumber(squareIdx, availableNums, invalidNums);
     } else {
       return randNum;
     }
   }
 
-  // TODO this is correct, but we need to backtrack to change a previous value, so our current value would be valid
-  // Have to keep track on what has been tried on which squares
-  // When backtracking, we would swap out the current square value to the old one
-  // and the old one would get a new value.
-  // If there are no possible combinations with the current and the previous value,
-  // we go back one index and run the same thing as below.
-
-  // Current index = x;
-  // Previous index = 9;
-  // 1. Check if the previous number would be valid in the current location
-  // 2. If not, we go back to the previous index.
-  // 3. If valid, we incrementally try to find the next number in the that location (7)
-  // 3. Switch the found number to the currently selected one (9->7 x->9)
-
-  const backtrack = () => {
-  }
-  
-  const generateUntilValid = (squareIdx, tempNums, invalidNums) => {
-    let randNum = generateRandNumber(squareIdx, tempNums, invalidNums); 
+  const generateUntilValid = (squareIdx, availableNums, invalidNums) => {
+    let randNum = generateRandNumber(squareIdx, availableNums, invalidNums);
     console.log('Generated: ' + randNum);
     const isValid = validate(squareIdx, randNum);
     if (!isValid) {
@@ -177,27 +162,46 @@ window.onload = () => {
       invalidNums = [...invalidNums, randNum];
       if (invalidNums.length === 9) {
         console.log('Exhausted all numbers');
-        return '_';
+        // When this actually works:
+        // Complete the puzzle before the time runs out
+        // When the timer ends, we disable all user interaction and show the completed sudoku (for the initial version) 
+        squares[squareIdx - 1] = 0;
+        return 0;
       } else {
-        randNum = generateUntilValid(squareIdx, tempNums, invalidNums);
+        return generateUntilValid(squareIdx, availableNums, invalidNums);
       }
-    }
+    } else {
+      return randNum;
+    }    
+  }
+
+  const generateNewNumber = (squareIdx, availableNums) => {
+    let invalidNums = [];
+    const randNum = generateUntilValid(squareIdx, availableNums, invalidNums);
+    invalidNums = [];
     return randNum;
   }
 
-  const generateNewNumber = (squareIdx, tempNums, availableNums) => {
-    let invalidNums = []; 
-    let randNum = generateUntilValid(squareIdx, tempNums, invalidNums);
-    console.log('squareIdx: ' + squareIdx);
-    squares[squareIdx] = randNum;
-    invalidNums = [];
-    tempNums = tempNums.filter(num => num != randNum);
-    if (tempNums.length === 0) {
-      tempNums = availableNums;
+  const fillWithInitialNumbers = (remainingIndexes, remainingValues, availableNums) => {
+    for (let i = 0; i < remainingIndexes.length; i++) {
+      const idx = remainingIndexes[i];
+      const generatedNum = generateNewNumber(idx, availableNums);
+      squares[idx] = generatedNum;
+      remainingValues[i] = generatedNum;
     }
+  }
 
-    console.log('Squares after generation');
-    console.log(squares);
+  const validateAfterGeneration = () => {
+    let errorMessages = [];
+    const start = new Date().getTime();
+    squares.forEach((value, index, array) => {
+      const isValid = validate(index, value);
+      if (!isValid) {
+        errorMessages.push('Invalid at index:' + index + '\r\n');
+      }
+    });
+    console.log(`Validation took: ${new Date().getTime() - start} ms`);
+    console.log('Error messages:' + errorMessages);
   }
   
   const generateRandomPuzzle = () => {
@@ -206,11 +210,6 @@ window.onload = () => {
       gridFillNums = [...gridFillNums, Math.ceil(Math.random() * 5 + 1)];
     }
     console.log('Filling with: ' + gridFillNums);
-    let availableNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let tempNums = availableNums;
-    for (let i = 0; i <= squares.length - 1; i++) {
-      generateNewNumber(squareIdx, tempNums, availableNums);
-    }
     const gridsForDisplay = grids.map((grid, index, arr) => {
       const numsToRemove = 9 - gridFillNums[index];
       for (let i = 0; i < numsToRemove; i++) {
@@ -219,16 +218,50 @@ window.onload = () => {
       }
       return grid;
     });
-    const indexesToFilter = gridsForDisplay.flatMap(grid => grid);
-    squares = squares.map(square => {
-      if (!indexesToFilter.includes(square)) {
+    
+    const remainingIndexesToFilter = gridsForDisplay.flatMap(grid => grid);
+    let remainingIndexes = [];
+    squares.forEach((value, idx, array) => {
+      if (!remainingIndexesToFilter.includes(idx)) {
+        remainingIndexes.push(idx);
+      }
+    });
+    
+    let remainingValues = [];
+    remainingValues.length = remainingIndexes.length;
+    remainingValues.fill(0);
+    while (remainingValues.includes(0)) {
+      fillWithInitialNumbers(remainingIndexes, remainingValues, availableNums);
+    }
+
+    console.log('Initial squares after generation');
+    console.log(squares);
+    validateAfterGeneration();
+
+    return;
+    
+    while (squares.includes(0)) {
+      for (let i = 0; i <= squares.length - 1; i++) {
+        if (squares[i] === 0) {
+          const generatedNum = generateNewNumber(i, availableNums);
+          squares[i] = generatedNum;
+          console.log('Squares after generation');
+          console.log(squares);
+        }
+        break;
+      }
+      break;
+    }
+    console.log('Square completed');
+    /*squares = squares.map((square, index, arr) => {
+      if (remainingIndexes.includes(index)) {
         return square;
       } else {
         return 0;
       }
-    });
+    });*/
   }
-  
+
   const setSquare = (square) => {
     if (currentlySelected !== null) {
       currentlySelected.style.backgroundColor = "#202020";
@@ -251,7 +284,7 @@ window.onload = () => {
       }
     }
   }
-  
+
   const sudokuContainer = document.querySelector('#sudoku-container');
 
   const loadPuzzle = async () => {
@@ -260,15 +293,15 @@ window.onload = () => {
       const row = document.createElement('div');
       for (let j = 0; j < 9; j++) {
         const square = document.createElement('span');
-        const squareNum = squares[i*9+j];
+        const squareNum = squares[i * 9 + j];
         square.innerHTML = squareNum === 0 ? '_' : squareNum;
-        square.setAttribute('index', i*9+j);
+        square.setAttribute('index', i * 9 + j);
         square.addEventListener('click', (square) => setSquare(square));
         row.appendChild(square);
       };
       sudokuPuzzle.appendChild(row);
     };
-    
+
     const sudokuControls = document.querySelector('#sudoku-controls');
     let prev = 0;
     for (let i = 1; i <= 3; i++) {
@@ -277,10 +310,10 @@ window.onload = () => {
         const square = document.createElement('span');
         prev++;
         square.innerHTML = prev;
-        square.setAttribute('control-row', i+1);
-        square.setAttribute('control-square', j+1);
+        square.setAttribute('control-row', i + 1);
+        square.setAttribute('control-square', j + 1);
         square.addEventListener('click', () => setSquareValue(square));
-      row.appendChild(square);
+        row.appendChild(square);
       };
       sudokuControls.appendChild(row);
     };
